e03693c951760b3d3c26938fce554e8abd833b28
Hayyaanshakeel <ayyanshakeel899@gmail.com>
2026-04-20 10:37:01 -0700

    middleware added in backend

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -1,5 +1,7 @@
 const express=require("express")
 const { authInsert, verifyOTP, login, resendOTP } = require("../controller/AuthController")
+const { authMiddleware } = require("../middleware/AuthMiddleware")
+const { authModel } = require("../models/AuthModel")
 
 const router=express.Router()
 
@@ -7,6 +9,19 @@ router.post("/register",authInsert)
 router.post("/verify-otp",verifyOTP)
 router.post("/login",login)
 router.post("/resend-otp",resendOTP)
+
+
+router.get("/dashboard", authMiddleware, (req, res) => {
+  res.json({
+    message: "Welcome to dashboard",
+    user: req.user,
+  });
+});
+router.get("/me", authMiddleware, async (req, res) => {
+  const user = await authModel.findById(req.user.id).select("-password");
+
+  res.json(user);
+});
 
 
 module.exports={router}
