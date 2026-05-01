6860bd348268c86f3ca9575157a1f147dc99e8e8
Hayyaanshakeel <ayyanshakeel899@gmail.com>
2026-05-01 00:04:29 -0700

    brevo connected in our sendEmail

--- app/utils/sendEmail.js
+++ app/utils/sendEmail.js
@@ -1,19 +1,21 @@
-const nodemailer=require ("nodemailer");
-require('dotenv').config()
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
 const sendEmail = async (to, subject, message) => {
   try {
     const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+
+    await transporter.verify();
+    console.log("SMTP connected successfully");
+
     await transporter.sendMail({
       from: `"Auth App" <${process.env.EMAIL}>`,
       to,
@@ -27,4 +29,5 @@ const sendEmail = async (to, subject, message) => {
     console.log("Email error:", error.message);
   }
 };
+
 module.exports = { sendEmail };
